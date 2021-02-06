3998fbb397d607badee9c1f28172f5b9d77e5299
Reverie <patlavovach@gmail.com>
2021-02-06 17:53:27 +0200

    component rename

---
+++ wh-combobox.js
@@ -0,0 +1,52 @@
+import {LitElement, html, css} from 'lit-element';
+
+export class WhCombobox extends LitElement {
+    static get styles() {
+        // language=HTML
+        return css`
+            :host {
+            display: block;
+            border: solid 1px gray;
+            padding: 16px;
+            max-width: 800px;
+            }
+        `;
+    }
+
+    static get properties() {
+        return {
+            /**
+             * The name to say "Hello" to.
+             */
+            name: {type: String},
+
+            /**
+             * The number of times the button has been clicked.
+             */
+
+            count: {type: Number},
+        };
+    }
+
+    constructor() {
+        super();
+        this.name = 'World';
+        this.count = 0;
+    }
+
+    render() {
+        return html`
+        <h1>Hello, ${this.name}!</h1>
+                <button @click=${this._onClick} part="button">
+        Click Count: ${this.count}
+      </button>
+      <slot></slot>
+    `;
+    }
+
+    _onClick() {
+        this.count++;
+    }
+}
+
+window.customElements.define('wh-combobox', WhCombobox);
